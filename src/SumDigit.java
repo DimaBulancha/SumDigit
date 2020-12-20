a31ce9068a93c0a1d09ebef7c740ba9ec222ebe6
DimaBulancha <dmitriybulancha@gmail.com>
2020-12-21 00:17:46 +0200

    Initial Commit

--- src/SumDigit.java
+++ src/SumDigit.java
@@ -2,7 +2,6 @@ import service.NumericService;
 
 public class SumDigit {
 
-
     public static void main(String[] args) {
         int result1 = NumericService.getSumByTypes(123456);
         int result2 = NumericService.getSumByMath(123456);
